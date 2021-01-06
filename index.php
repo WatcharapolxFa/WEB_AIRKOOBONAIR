8b007302a5ee6bf27cfd8a9a0072906641e009e8
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-01-06 13:28:06 +0700

    home

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
     <head>
         <title>koobonair.com</title>
         <meta charset="utf-8">
@@ -15,7 +16,8 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     </head>
-    <body style="background-color: #727272;   font-family:Caveat">
+
+    <body style="background-color: #727272;   font-family:Caveat ">
         <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
             <a class="navbar-brand" href="index.html">
                 <img src="bg/bg1.png" width="50px">
@@ -24,147 +26,38 @@
             <ul class="navbar-nav">
             &nbsp;&nbsp;
                 <li class="nav-item">
-                    <a class="nav-link" href="index.html">สินค้าราคาโปรโมชั่น</a>
+                    <a class="nav-link" href="index.html">HOME</a>
                 </li>&nbsp;&nbsp;
                 <li class="nav-item">
-                    <a class="nav-link" href="member.html">สินค้าทั้งหมดของเรา</a>
+                    <a class="nav-link" href="member.html">ABOUT</a>
                 </li>&nbsp;&nbsp;
                 <li class="nav-item">
-                    <a class="nav-link" href="gallery.html">สินค้าผ่อน 0 %</a>
+                    <a class="nav-link" href="gallery.html">INSTALLATION COST</a>
                 </li>&nbsp;&nbsp;
                 <li class="nav-item">
-                    <a class="nav-link" href="gallery.html">บริการของเรา</a>
+                    <a class="nav-link" href="gallery.html">PAYMENT METHOD</a>
                 </li>&nbsp;&nbsp;
-                <li class="nav-item">
-                    <a class="nav-link" href="gallery.html">ช่องทางการติดต่อ</a>
-                </li>
+                <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              AIR
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                              <a class="dropdown-item" href="#">WALL TYPE</a>
+                              <a class="dropdown-item" href="#">CEILING TYPE</a>
+                                <a class="dropdown-item" href="#">PACKAGE TYPE</a>
+                              <div class="dropdown-divider"></div>
+                              <a class="dropdown-item" href="#">member n</a>
+                                </div>
+                        </li>
+
                 
             </ul>
             <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="ระบุรุ่นที่ท่านต้องการ" aria-label="Search">
+                <input class="form-control mr-sm-2" type="search" placeholder="ค้นหา" aria-label="Search">
                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">ค้นหา</button>&nbsp;&nbsp;
                 <a href="login.php" class="btn btn-warning text-white"> เข้าสู่ระบบ</a>
             </form>
-        </div>
-        </nav>
-        <div class="container">
-            <br>
-            <div class="row">
-                
-                <img src="bg/bg2.png" class="img-responsive" style="margin-top: 60px;">
-            </div>
-
-            <div class="row">
-                <div class="col-md-6">
-                    <br>
-                   <div class="card">
-                       <div class="card-header ">
-                        
-                            <strong style="font-size: 24px; " >
-                               DAIKIN 
-                            </strong>
-                            <br>
-                            <br>
-                            <img src="bg/1234.jpg" alt="" class="img-responsive">
-                       </div>
-                       <div class="card-body">
-                            <table class="table table-hover"> 
-                                <thead class="text-white" style="background-color: #21B699;"> 
-                                    <th>รุ่น</th>
-                                    <th>BTU</th> 
-                                    <th>ราคาส่ง</th>  
-
-                                </thead>
-                                <tbody style="background-color :rgb(154, 183, 236);">
-                                    <td>MSY-KS09VF#5</td>
-                                    <td>8900</td>
-                                    <td>10,900.-</td>
-                                </tbody>
-                                <tbody style="background-color: rgb(73, 128, 230)";>
-                                    <td>MSY-KS13VF#5</td>
-                                    <td>12000</td>
-                                    <td>13,400.-</td>
-                                </tbody>
-                                <tbody style="background-color: rgb(154, 183, 236);">
-                                    <td>MSY-KS15VF#5</td>
-                                    <td>14400</td>
-                                    <td>17,500.-</td>
-                                </tbody>
-                                <tbody style="background-color: rgb(73, 128, 230)";>
-                                    <td>MSY-KS18VF#5</td>
-                                    <td>17100</td>
-                                    <td>21,500.- </td>
-                                    <tbody style="background-color: rgb(154, 183, 236);">
-                                    <td>MSY-KS15VF#5</td>
-                                    <td>14400</td>
-                                    <td>17,500.-</td>
-                                </tbody>
-                                <tbody style="background-color: rgb(73, 128, 230)";>
-                                    <td>MSY-KS24VF#5</td>
-                                    <td>22000</td>
-                                    <td>34,800.-</td>
-                                </tbody>
-                                </tbody>
-                            </table>
-                       </div>
-                   </div>
-                    
-                </div>
-                <div class="col-md-6">
-                    <br>
-                   <div class="card">
-                       <div class="card-header">
-                        
-                            <strong style="font-size: 18px;">
-                                แอร์มิตซูบิชิเฮฟวี่ดิวตี้ Japan Quality
-                            </strong>
-                            <img src="bg/456.jpg" alt="" class="img-responsive">
-                       </div>
-                       
-                        <div class="card-body">
-                            <table class="table table-hover"> 
-                                <thead class="text-white" style="background-color: blueviolet;"> 
-                                    <th>รุ่น</th>
-                                    <th>BTU</th> 
-                                    <th>ราคาส่ง</th>  
-
-                                </thead>
-                                <tbody style="background-color: rgb(154, 183, 236);">
-                                    <td>SRK-10CVV-W1 #5</td>
-                                    <td>9000</td>
-                                    <td>10,500.-</td>
-                                </tbody>
-                                <tbody style="background-color: rgb(73, 128, 230)";>
-                                    <td>SRK-13CVV-W1 #5</td>
-                                    <td>13000</td>
-                                    <td>12,500.-</td>
-                                </tbody>
-                                <tbody style="background-color: rgb(154, 183, 236);">
-                                    <td>SRK-15CVV-W1 #5</td>
-                                    <td>15000</td>
-                                    <td>15,200.-</td>
-                                </tbody>
-                                <tbody style="background-color: rgb(73, 128, 230)";>
-                                    <td>SRK-18CVV-W1 #5</td>
-                                    <td>18000</td>
-                                    <td>19,200.- </td>
-                                    <tbody style="background-color: rgb(154, 183, 236);">
-                                    <td>SRK-24CVV-W1 #5</td>
-                                    <td>24000</td>
-                                    <td>27,200.-</td>
-                                </tbody>
-                                
-                                </tbody>
-                            </table>
-                       </div>
-                    </div>
-                   
-                    
-                </div>
-            </div>
-
-            <br>
-        </div>
+       
         
     </body>
 
