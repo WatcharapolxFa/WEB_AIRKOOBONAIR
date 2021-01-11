770c4f12220954501d99e4dd1de1eb5c42047e5f
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-01-11 16:26:12 +0700

    0pi

--- index.php
+++ index.php
@@ -12,29 +12,26 @@
         <script src="js/bootstrap.js"></script> 
         <script src="js/bootstrap.min.js"></script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     </head>
-    <body style="background-color: #727272;   font-family:Caveat ">
+    <body style="background-color: #727272;">
         <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
-            <a class="navbar-brand" href="index.html">
+            <a class="navbar-brand" href="index.php">
                 <img src="bg/bg1.png" width="50px">
             </a>
             <div class="collapse navbar-collapse container-fluid">
             <ul class="navbar-nav">
             &nbsp;&nbsp;
                 <li class="nav-item">
-                    <a class="nav-link" href="index.html">HOME</a>
+                    <a class="nav-link" href="index.php">HOME</a>
                 </li>&nbsp;&nbsp;
                 <li class="nav-item">
-                    <a class="nav-link" href="member.html">ABOUT</a>
+                    <a class="nav-link" href="member.php">ABOUT</a>
                 </li>&nbsp;&nbsp;
                 <li class="nav-item">
-                    <a class="nav-link" href="gallery.html">INSTALLATION COST</a>
+                    <a class="nav-link" href="gallery.php">INSTALLATION COST</a>
                 </li>&nbsp;&nbsp;
                 <li class="nav-item">
-                    <a class="nav-link" href="gallery.html">PAYMENT METHOD</a>
+                    <a class="nav-link" href="gallery.php">PAYMENT METHOD</a>
                 </li>&nbsp;&nbsp;
                 <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -57,7 +54,8 @@
                 <a href="login.php" class="btn btn-warning text-white"> เข้าสู่ระบบ</a>
             </form>
        
-        
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>   
     </body>
-
 </html>
